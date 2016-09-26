4ec16c4436b7c4c9dd2c18993ef087f5dba10c3f
Ghaz-Siddique <ghaz_siddique@yahoo.com>
2016-09-26 11:40:16 +0500

    Adding Sq.root calculator

---
+++ app.js
@@ -0,0 +1,17 @@
+var num1 = prompt("Enter 1st number");
+
+var op1 = prompt("Enter operator", "Here operator sign is $");
+
+
+
+var int1 = parseFloat(num1);
+
+
+
+if (op1 == "$") {
+
+    
+    
+    document.write(Math.sqrt(int1))
+}
+
